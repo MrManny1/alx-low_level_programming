0fb6393785fe38fdf080178a6cd74b19a0648c62
Emmanuel <oladepogboyega@gmail.com>
2023-02-17 15:18:01 -0800

    Insomnia problem

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Insomnia is bad
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char a = 'c';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
