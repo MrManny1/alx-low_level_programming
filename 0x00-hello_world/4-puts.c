d105182f0e75f4e33d1189448b0dccf332fa58de
Emmanuel <oladepogboyega@gmail.com>
2023-02-16 19:33:43 -0800

    Hello puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
- * main - print a line code using puts
+/**
+ * main -Print a line code using puts
  *
- *
- * return: 0
- */
+ * Return: 0
+*/
+
 int main(void)
-{     
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}    
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
