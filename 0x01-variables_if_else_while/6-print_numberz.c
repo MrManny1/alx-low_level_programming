1c0c6a61a2df42e6d9ad3ed9b7ff8faad9eda7fa
Emmanuel <oladepogboyega@gmail.com>
2023-02-17 16:28:16 -0800

    The Numberz made it

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - numberz done
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+	}
+
+	putchar('\n');
+	return (0);
+}
