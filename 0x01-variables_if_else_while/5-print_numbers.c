d720a2b7acf27756b90a209028350678a5908c8f
Emmanuel <oladepogboyega@gmail.com>
2023-02-17 16:22:37 -0800

    My numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print a random num
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
