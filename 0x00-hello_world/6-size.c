963ef1b789cca13df4c93785cd4b81b4f3814821
Emmanuel <oladepogboyega@gmail.com>
2023-02-20 06:33:49 -0800

    various types in C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    printf("Size of char: %ld byte(s)\n", sizeof(char));
-    printf("Size of short: %ld byte(s)\n", sizeof(short));
-    printf("Size of int: %ld byte(s)\n", sizeof(int));
-    printf("Size of long: %ld byte(s)\n", sizeof(long));
-    printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-    printf("Size of float: %ld byte(s)\n", sizeof(float));
-    printf("Size of double: %ld byte(s)\n", sizeof(double));
-    printf("Size of long double: %ld byte(s)\n", sizeof(long double));
-    return 0;
+/**
+ * main - A C program the size of various types on the computer
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	if __WORDSIZE == 64
+		printf("This is a 64-bit machine.\n");
+	else
+		printf("This is a 32-bit machine.\n";
+	endif
+
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of short: %ld byte(s)\n", sizeof(short));
+	printf("Size of long: %ld byte(s)\n", sizeof(long));
+	printf("Size of int: %ld byte(s)\n', size of(int));
+	return 0:}
 }
