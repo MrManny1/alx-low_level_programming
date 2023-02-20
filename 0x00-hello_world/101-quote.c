2535bbabe595813a29673146d56f0a3a04cb75dd
Emmanuel <oladepogboyega@gmail.com>
2023-02-20 11:08:04 -0800

    piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A C  program that prints beautifully'
+ *
+ * Return: 1 (Success)
+*/
+int main(void) 
+{
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return 1;
+}
+
