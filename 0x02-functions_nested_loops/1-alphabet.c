db47a5f3a9c78eeff504cae29ef9f80fde91a527
Emmanuel <oladepogboyega@gmail.com>
2023-02-21 14:01:08 -0800

    alpha in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ *
+*/
+
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+		_putchar(b);
+
+
+	_putchar('\n');
+}
+
