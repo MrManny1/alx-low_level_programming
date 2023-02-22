ec0115e07c1777462918add5f8dc3f5fd59549b7
Emmanuel <oladepogboyega@gmail.com>
2023-02-22 06:12:25 -0800

    my 9 times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * jack_bauer - a function that prints every minute of the day of Jack Bauer
  *
@@ -8,22 +8,18 @@
 
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
-	{
-		for (int minute = 0; minute < 60; minute++)
-		{
-			int tens_hour = hour / 10;
-			int ones_hour = hour % 10;
-			int tens_minute = minute / 10;
-			int ones_minute = minute % 10;
+	int hour, minute;
 
-			_putchar('0' + tens_hour);
-			_putchar('0' + ones_hour);
-		       	_putchar(':');
-			_putchar('0' + tens_minute);
-			_putchar('0' + ones_minute);
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) +'0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
-			return (0);
 		}
 	}
 }
