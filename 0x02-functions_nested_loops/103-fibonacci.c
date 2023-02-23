bfbe249298edd9a1f29eaa3f6489c7e135899f39
Emmanuel <oladepogboyega@gmail.com>
2023-02-22 17:18:07 -0800

    even valued terms printed here

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fibonacci Sequence
+ * Description : prints the sum of the even-valued terms,
+ * followed by a new line
+ * Return: 0
+*/
+int main(void)
+{
+	int c = 0, d = 1, next = 0;
+	int sum = 0;
+	
+	while (next < 4000000)
+	{
+		next = c + d;
+		c = d;
+		d = next;
+		if (next % 2 == 0)
+			sum += next;
+	}printf("%i\n", sum);
+	return (0);
+}
