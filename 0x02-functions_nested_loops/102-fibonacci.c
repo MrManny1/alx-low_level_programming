1454ec507daa5ca66c1c67b7125d6be99ea0abfe
Emmanuel <oladepogboyega@gmail.com>
2023-02-22 17:00:13 -0800

    50 Fibonacci for stealing passwords

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fibonacci numbers
+ * description : prints the first 50 Fibonacci numbers starting with 1 and 2
+ * followed by a new line
+ * Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+	long int f = 0, g = 1, next;
+
+	while (c < 50)
+	{
+		next = f + g;
+		f = g;
+		g = next;
+		printf("%lu", next);
+
+		if (c < 49)
+		{
+			print(", ");
+		}
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
