b86af65658886e52f2b3f60cdc591cd6ac4aa40f
Emmanuel <oladepogboyega@gmail.com>
2023-02-23 23:47:21 -0800

    line ending with a new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line in the terminal
+ * @n : the number of lines to draw
+ * Return: 0
+ *
+*/
+void print_line(int n)
+{
+
+	int c;
+
+	if (n <= 0)
+	{
+		_putchr('\n');
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
