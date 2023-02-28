6d568a7577d23ed06b338b980c753fa51c3ea26a
Emmanuel <oladepogboyega@gmail.com>
2023-02-27 19:54:56 -0800

    the lies they tell

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 -  a function that prints every other character of a string
+ * @str: The string
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
