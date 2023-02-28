5514670a9478e4f675d4c898f3cf05af14ab1caa
Emmanuel <oladepogboyega@gmail.com>
2023-02-27 23:55:36 -0800

    Destination and Source value

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,4 +11,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int a;
-	for (a = 0; src[a] != '\0'
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a++] = '\0';
+	return (dest);
+}
