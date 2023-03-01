2f54e1418bec01407d8a0f9592ced301c03edd0a
Emmanuel <oladepogboyega@gmail.com>
2023-03-01 13:04:19 -0800

    lowercase to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a
+ * string to uppercase
+ * @n: pointer
+ * Return: n
+ */
+
+char *string_toupper(char *)
+{
+	int c;
+
+	c = 0;
+	while (n[c] != '\0')
+	{
+		if (n[c] >= 'a' && n[c] <= 'z')
+			n[c] = n[c] - ('a' - 'A');
+		c++;
+	}
+	return (n);
+}
