846c41defc86e65b8734d04b213baa9c2dc4330b
Emmanuel <oladepogboyega@gmail.com>
2023-03-08 12:24:26 -0800

    3 factorial done

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,19 +11,16 @@
 
 int factorial(int n)
 {
-	int result = 1;
-	int i = 1;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-
-		while (i <= n)
-		{
-			result *= i;
-			i++;
-		}
-
-		return (result);
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
